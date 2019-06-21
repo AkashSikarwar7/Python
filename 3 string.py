afa3cd8bbb0711f2d299d2ef6fd5bc64fa431656
AkashSikarwar7 <47856212+AkashSikarwar7@users.noreply.github.com>
2019-06-21 17:46:19 +0530

    Add files via upload

---
+++ 3 string.py
@@ -0,0 +1,17 @@
+a='string1\''
+b='string2'
+c='string3'
+d=5
+print('Length of a = '),print(len(a))
+print('Length of b = ')
+print(len(b))
+print('Length of c = ',len(c))
+
+print('-----------------------')
+
+print(a+" "+b+" "+c)
+print(a*(10))
+
+print('-----------------------')
+
+print(a+" "+str(d))
